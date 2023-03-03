068530533e63cf2cc78e635325a721eac3681f70
ayatbd <ayatbd.571@gmail.com>
2023-03-03 17:26:49 +0600

    third git commit

--- js/index.js
+++ js/index.js
@@ -5,11 +5,13 @@ const loadHubs = async() =>{
     displayHubs(data.data.tools);
 }
 
+// displaying all hubs 
+
 const displayHubs = hubs =>{
     // console.log(hubs);
     const hubsContainer = document.getElementById('hubs-container');
     for(const data of hubs){
-        console.log(data);
+        console.log(data.features);
 
         const hubsDiv  = document.createElement('div');
         hubsDiv.classList.add("card", "w-96", "bg-base-100", "shadow-xl", "border", "p-3");
@@ -18,14 +20,10 @@ const displayHubs = hubs =>{
             <figure><img src="${data.image}" alt="Shoes" / ></figure>
             <div class="card-body">
                 <h2 class="card-title">Features</h2>
-                <p id="order-list" class="mb-3">
-                    ${data.features.forEach(data =>{
-                        const orderList = document.getElementById('order-list');
-                        const ul = document.createElement('ul');
-                        ul.innerHTML = `data`
-                        orderList.appendChild(ul);
-                        
-                    })}
+                <p class="mb-3 text-left">
+                    <ol id="${data.id}" class="list-decimal text-left list list-inside">
+
+                    </ol>
                 </p>
                 <div class="card-footer border-t flex justify-between items-center pt-3">
                     <div>
@@ -34,18 +32,25 @@ const displayHubs = hubs =>{
                             <i class="fa-solid fa-calendar-days"></i> ${data.published_in}
                         </div>
                     </div>
-                    <button class="bg-red-300 text-red-500 p-1 rounded-full hover:bg-slate-200 fa-solid fa-arrow-right"></button>
+                    <button onclick="" class="bg-red-300 text-red-500 p-1 rounded-full hover:bg-slate-200 fa-solid fa-arrow-right"></button>
                 
                 </div>
-
-                
-                
             </div
         `;
         hubsContainer.appendChild(hubsDiv);
 
+        // features list items appending
+        const featureContainer = document.getElementById(data.id);
+            data.features.forEach( (featur) => {
+                const featureList = document.createElement('li');
+                featureList.innerText = featur;
+                featureContainer.appendChild(featureList);
+            })
+
     }
     // datas.forEach(data =>{});
 }
+
+
 
 loadHubs ();
